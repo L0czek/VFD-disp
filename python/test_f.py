d2247e744cfe80f7adc3c1300495daac08f84bfe
Artur Puzio <contact@puzio.waw.pl>
2026-09-08 22:11:39 +0200

    julka tu byla

---
+++ python/test_f.py
@@ -0,0 +1,46 @@
+from machine import *
+import time
+adc_f = ADC(2)
+adc_hv = ADC(3)
+
+disable_f = Pin(6, Pin.OUT, value=1) 
+disable_hv = Pin(9, Pin.OUT, value=1)
+
+
+spi = SPI(
+    1,
+    baudrate=100_000,
+    polarity=0,
+    phase=0,
+    sck=Pin(19),
+    mosi=Pin(18),
+)
+
+# also known as "strobe"
+latch = Pin(21, Pin.OUT, value=0)
+blanking = Pin(20, Pin.OUT, value=1)
+
+def write_shift_register(value):
+    if len(value) != 5:
+        print("gowno dales")
+    spi.write(bytes(value))
+    latch.on()
+    time.sleep_ms(1)
+    latch.off()
+    print("Wrote", value)
+
+#write_shift_register([0xAA]*5)
+
+def print_voltages():
+    print("HV: ", adc_hv.read_uv()*101.0/1000000)
+    print("F: ", adc_f.read_uv()*101.0/1000000)
+
+print_voltages()
+time.sleep(1)
+disable_f.value(0)
+#disable_hv.value(0)
+
+for i in range(7):
+    time.sleep(1)
+    print_voltages()
+
